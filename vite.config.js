9a387d7e80a719ad0fc537fd411651bd3b153ddb
Hendri Panjaitan <hendripanjaitan12@gmail.com>
2026-05-07 15:28:41 +0700

    Fix: Conditional base for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/tobatrip/", 
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // Jika sedang build untuk GitHub Pages (npm run deploy), pakai base /tobatrip/
+    // Jika tidak (untuk Vercel atau lokal), pakai base /
+    base: process.env.NODE_ENV === 'production' && command === 'build' ? '/tobatrip/' : '/',
+  }
 })
